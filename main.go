cc9b7bd85866de8a435e073d9c6921fd377c4a09
Pablo Tilotta <ptilotta@aper.com>
2023-05-07 21:02:15 -0300

    ultimo commit

--- main.go
+++ main.go
@@ -63,15 +63,18 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) 
 
 	fmt.Println("Sali de Manejadores")
 	if respAPI.CustomResp == nil {
-		respAPI.CustomResp.Headers = map[string]string{
+		response := &events.APIGatewayProxyResponse{
+			StatusCode: respAPI.Status,
+			Body:       respAPI.Message,
+			Headers:    make(map[string]string),
+		}
+		response.Headers = map[string]string{
 			"Content-Type": "application/json",
 		}
-		respAPI.CustomResp.StatusCode = respAPI.Status
-		respAPI.CustomResp.Body = string(respAPI.Message)
+		return response, nil
+	} else {
+		return respAPI.CustomResp, nil
 	}
-
-	fmt.Println("voy al último RETURN")
-	return respAPI.CustomResp, nil
 }
 
 func main() {
